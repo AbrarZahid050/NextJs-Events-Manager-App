966c1b2c091bf742d16f77175e84e0e55c7587d9
AbrarZahid050 <abrar.zahid050@gmail.com>
2022-11-10 21:30:59 +0500

    dynamically adding ids

--- pages/testing/[pid].jsx
+++ pages/testing/[pid].jsx
@@ -6,20 +6,24 @@ const detailedProduct = (props) => {
   const { loadedProduct } = props;
   return (
     <Fragment>
-      <h1>{loadedProduct.desc}</h1>
+      <h1>{loadedProduct.product}</h1>
+      <p>{loadedProduct.desc}</p>
     </Fragment>
   );
+};
+
+const getData = async () => {
+  const filePath = path.join(process.cwd(), "public", "testing.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
 };
 
 export async function getStaticProps(context) {
   const { params } = context;
   const prodId = params.pid;
-  console.log(prodId);
 
-  const filePath = path.join(process.cwd(), "public", "testing.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-
+  const data = await getData();
   const product = data.products.find((product) => product.id === prodId);
 
   return {
@@ -30,13 +34,15 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const id = data.products.map((product) => {
+    return { params: { pid: `${product.id}` } }; //Dynamically adding the id's which needs to be preRendered.
+  });
+
   return {
-    paths: [
-      { params: { pid: "u1" } },
-      { params: { pid: "u2" } },
-      { params: { pid: "u3" } },
-    ],
-    fallback: false,
+    paths: id,
+    fallback: "blocking",
   };
 }
 
